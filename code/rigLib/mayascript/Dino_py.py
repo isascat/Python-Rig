d281128aa35259e2a50d469076be3878cb655e23
isascat <33879207+isascat@users.noreply.github.com>
2018-01-24 13:51:12 -0800

    Delete Dino_py.py

--- code/rigLib/mayascript/Dino_py.py
+++
@@ -1,8 +0,0 @@
-import rigLib
-
-reload(rigLib.base.control)
-spine = rigLib.base.control.Control(prefix = 'spine1')
-
-reload(rigLib.base.module)
-
-m = rigLib.base.module.Base( characterName = 'Dino_model')
